b91c312287605ba1069ecd8e7e39005ac1625bd9
Olga <hloesilent@gmail.com>
2019-07-29 01:50:54 +0300

    домашка 11

---
+++ lesson-11/2.js
@@ -0,0 +1,40 @@
+/**
+ * Задача 2.
+ *
+ * Создайте функцию createFibonacciGenerator(),
+ * которая вернёт ещё одну функцию,
+ * каждый вызов которой будет возвращать число из последовательности Фибоначчи.
+ *
+ * Условия:
+ * - Задачу нужно решить с помощью замыкания.
+ */
+
+// Решение
+
+const createFibonacciGenerator = function () {
+    let a = 0;
+    let b = 1;
+    let c;
+    const innerFibonacci = function () {
+        c = a + b;
+        a = b;
+        b = c;
+
+        return c;
+    }
+
+    return innerFibonacci;
+
+}
+
+const generateFibonacciNumber = createFibonacciGenerator();
+
+console.log(generateFibonacciNumber()); // 1
+console.log(generateFibonacciNumber()); // 1
+console.log(generateFibonacciNumber()); // 2
+console.log(generateFibonacciNumber()); // 3
+console.log(generateFibonacciNumber()); // 5
+console.log(generateFibonacciNumber()); // 8
+console.log(generateFibonacciNumber()); // 13
+
+exports.createFibonacciGenerator = createFibonacciGenerator;

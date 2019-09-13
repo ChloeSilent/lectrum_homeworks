8115ada31bd185cf168fcdb39abaed3a970a6abe
Olga <hloesilent@gmail.com>
2019-09-13 16:55:35 +0300

    старт

---
+++ personal_prj/project.js
@@ -0,0 +1,84 @@
+class Transaction {
+
+    constructor() {
+        this.store = new Map();
+        this.logs = [];
+
+    }
+
+
+    async dispatch(scrnario) {
+
+            try {
+                await scrnario.call();
+                // this.map.set( /*данные, необходимые для выполнения следующего шага сценария */ );
+                // this.logs.push( /*Index, meta, stepResult/nextStep, error */ )
+            } catch (error) {
+                console.log(error.message);
+                rollback(scrnario)
+
+            }
+
+    }
+
+    async rollback(scrnario) {
+        try {
+            await scrnario.restore();
+            // this.map.set( /*данные, необходимые для выполнения следующего шага сценария */ );
+            // this.logs.push( /*Index, meta, nextStep, error */ )
+        } catch (error) {
+            console.log(error.message);
+            // this.logs.push( /*Index, meta, nextStep, error */ )
+        }
+    }
+
+}
+
+const scenario = [{
+        index: 1,
+        meta: {
+            title: 'Collect backup information.',
+            description: 'Collects pieces of data that required for restore scenario',
+        },
+        async call(store, logs) {
+            // Логика выполнения шага
+        },
+        async restore(store, logs) {
+            // Логика отката шага
+        },
+    },
+    {
+        index: 2,
+        meta: {
+            title: 'Withdraw funds from source account.',
+            description: 'Takes off funds from source account and freezes it until entire scenario ends successfully or unsuccessfully.',
+        },
+        async call(store, logs) {
+            // Логика выполнения шага
+        },
+        async restore(store, logs) {
+            // Логика отката шага
+        },
+    },
+];
+
+
+const transaction = new Transaction();
+
+
+(async () => {
+    try {
+        await transaction.dispatch(scenario);
+
+        const {
+            store,
+            logs,
+            status
+        } = transaction;
+        log(store);
+        log(logs);
+        log(status);
+    } catch (error) {
+        // Send email about broken transaction
+    }
+})();
